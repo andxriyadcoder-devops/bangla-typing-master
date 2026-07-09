97a0c73ebe49d01c4df5d44fadabd71b36c93878
andxriyadcoder-devops <andxriyadcoder@gmail.com>
2026-07-09 21:53:54 +0600

    Initial project setup

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <main className="min-h-screen bg-slate-950 text-white">
+      <h1 className="pt-20 text-center text-5xl font-bold">
+        Bangla Typing Master Pro 🚀
+      </h1>
+
+      <p className="mt-6 text-center text-slate-400">
+        Sprint 1 Successfully Started
+      </p>
+    </main>
+  );
+}
+
+export default App;
