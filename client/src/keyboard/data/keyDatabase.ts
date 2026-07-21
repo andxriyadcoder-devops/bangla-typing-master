42224456c6bf253bc1790d3b510bc7a95304fb4f
andxriyadcoder-devops <andxriyadcoder@gmail.com>
2026-07-21 22:27:56 +0600

    feat(keyboard): add function row database

--- client/src/keyboard/data/keyDatabase.ts
+++ client/src/keyboard/data/keyDatabase.ts
@@ -1,3 +1,125 @@
 import type { KeyboardKeyData } from "../types/keyboard";
 
-export const keyDatabase: KeyboardKeyData[] = [];
+export const keyDatabase: KeyboardKeyData[] = [
+  // ==========================
+  // Function Row
+  // ==========================
+
+  {
+    id: "Escape",
+    code: "Escape",
+    primary: "Esc",
+    width: 1,
+    finger: "left-pinky",
+    hand: "left",
+    isModifier: false,
+  },
+
+  {
+    id: "F1",
+    code: "F1",
+    primary: "F1",
+    width: 1,
+    finger: "left-pinky",
+    hand: "left",
+  },
+
+  {
+    id: "F2",
+    code: "F2",
+    primary: "F2",
+    width: 1,
+    finger: "left-ring",
+    hand: "left",
+  },
+
+  {
+    id: "F3",
+    code: "F3",
+    primary: "F3",
+    width: 1,
+    finger: "left-middle",
+    hand: "left",
+  },
+
+  {
+    id: "F4",
+    code: "F4",
+    primary: "F4",
+    width: 1,
+    finger: "left-index",
+    hand: "left",
+  },
+
+  {
+    id: "F5",
+    code: "F5",
+    primary: "F5",
+    width: 1,
+    finger: "left-index",
+    hand: "left",
+  },
+
+  {
+    id: "F6",
+    code: "F6",
+    primary: "F6",
+    width: 1,
+    finger: "right-index",
+    hand: "right",
+  },
+
+  {
+    id: "F7",
+    code: "F7",
+    primary: "F7",
+    width: 1,
+    finger: "right-index",
+    hand: "right",
+  },
+
+  {
+    id: "F8",
+    code: "F8",
+    primary: "F8",
+    width: 1,
+    finger: "right-middle",
+    hand: "right",
+  },
+
+  {
+    id: "F9",
+    code: "F9",
+    primary: "F9",
+    width: 1,
+    finger: "right-ring",
+    hand: "right",
+  },
+
+  {
+    id: "F10",
+    code: "F10",
+    primary: "F10",
+    width: 1,
+    finger: "right-pinky",
+    hand: "right",
+  },
+
+  {
+    id: "F11",
+    code: "F11",
+    primary: "F11",
+    width: 1,
+    finger: "right-pinky",
+    hand: "right",
+  },
+
+  {
+    id: "F12",
+    code: "F12",
+    primary: "F12",
+    width: 1,
+    finger: "right-pinky",
+    hand: "right",
+  },
+];
