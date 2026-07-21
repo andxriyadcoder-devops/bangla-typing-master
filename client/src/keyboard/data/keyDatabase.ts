5aaceb865a9d03a481ba9a9164c4ea6770ebd7f7
andxriyadcoder-devops <andxriyadcoder@gmail.com>
2026-07-21 22:33:18 +0600

    feat(keyboard): add number row database

--- client/src/keyboard/data/keyDatabase.ts
+++ client/src/keyboard/data/keyDatabase.ts
@@ -122,4 +122,147 @@ export const keyDatabase: KeyboardKeyData[] = [
     finger: "right-pinky",
     hand: "right",
   },
+    // ==========================
+  // Number Row
+  // ==========================
+
+  {
+    id: "Backquote",
+    code: "Backquote",
+    primary: "`",
+    shift: "~",
+    width: 1,
+    finger: "left-pinky",
+    hand: "left",
+  },
+
+  {
+    id: "Digit1",
+    code: "Digit1",
+    primary: "1",
+    shift: "!",
+    width: 1,
+    finger: "left-pinky",
+    hand: "left",
+  },
+
+  {
+    id: "Digit2",
+    code: "Digit2",
+    primary: "2",
+    shift: "@",
+    width: 1,
+    finger: "left-ring",
+    hand: "left",
+  },
+
+  {
+    id: "Digit3",
+    code: "Digit3",
+    primary: "3",
+    shift: "#",
+    width: 1,
+    finger: "left-middle",
+    hand: "left",
+  },
+
+  {
+    id: "Digit4",
+    code: "Digit4",
+    primary: "4",
+    shift: "$",
+    width: 1,
+    finger: "left-index",
+    hand: "left",
+  },
+
+  {
+    id: "Digit5",
+    code: "Digit5",
+    primary: "5",
+    shift: "%",
+    width: 1,
+    finger: "left-index",
+    hand: "left",
+  },
+
+  {
+    id: "Digit6",
+    code: "Digit6",
+    primary: "6",
+    shift: "^",
+    width: 1,
+    finger: "right-index",
+    hand: "right",
+  },
+
+  {
+    id: "Digit7",
+    code: "Digit7",
+    primary: "7",
+    shift: "&",
+    width: 1,
+    finger: "right-index",
+    hand: "right",
+  },
+
+  {
+    id: "Digit8",
+    code: "Digit8",
+    primary: "8",
+    shift: "*",
+    width: 1,
+    finger: "right-middle",
+    hand: "right",
+  },
+
+  {
+    id: "Digit9",
+    code: "Digit9",
+    primary: "9",
+    shift: "(",
+    width: 1,
+    finger: "right-ring",
+    hand: "right",
+  },
+
+  {
+    id: "Digit0",
+    code: "Digit0",
+    primary: "0",
+    shift: ")",
+    width: 1,
+    finger: "right-pinky",
+    hand: "right",
+  },
+
+  {
+    id: "Minus",
+    code: "Minus",
+    primary: "-",
+    shift: "_",
+    width: 1,
+    finger: "right-pinky",
+    hand: "right",
+  },
+
+  {
+    id: "Equal",
+    code: "Equal",
+    primary: "=",
+    shift: "+",
+    width: 1,
+    finger: "right-pinky",
+    hand: "right",
+  },
+
+  {
+    id: "Backspace",
+    code: "Backspace",
+    primary: "Backspace",
+    width: 2,
+    finger: "right-pinky",
+    hand: "right",
+    isModifier: true,
+  },
 ];
